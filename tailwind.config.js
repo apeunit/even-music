cb11fb695395bdd5aca96a60d815e76b71961b9f
Odile078 <ouwimpuhwe620@daviscollege.com>
2023-01-23 15:22:32 +0200

    feat: create typography components

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,26 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: "#111827", // dark gray
+      },
       fontFamily: {
         sans: ["var(--font-inter)", ...fontFamily.sans],
+      },
+      fontSize: {
+        "1xl": "1.4375rem", // 23px
+        "3.5xl": "2rem", // 32px
+      },
+      letterSpacing: {
+        thin: "0.0859375rem", // 1px
+        thinner: "0.0625rem", // 1.375px
+        thinnest: "0.1125rem", // 1.8px
+      },
+      lineHeight: {
+        3.75: "0.9375rem", // 15px
+        4.25: "1.0625rem", // 17px
+        4.75: "1.1875rem", // 19px
+        5.5: "1.375rem", // 22px
       },
     },
   },
