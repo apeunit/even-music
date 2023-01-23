720653e1132b969db3d912cd8f4d6e20ecb4279e
Fidesnoella <fniragena@gmail.com>
2023-01-23 19:29:09 +0200

    fix: modify link underline

--- components/ui/Anchor.jsx
+++ components/ui/Anchor.jsx
@@ -5,7 +5,7 @@ export default function Anchor({ children, url }) {
         <Link
             href={url}
             target="_blank"
-            className="block font-medium sm:text-sm text-xs -tracking-tight leading-3.75 sm:leading-4.25 text-black underline underline-offset-3 hover:text-secondary"
+            className="inline-block font-medium sm:text-sm text-xs -tracking-tight leading-3.75 sm:leading-4.25 text-black underline underline-offset-3 hover:text-secondary"
         >
             {children}
         </Link>
