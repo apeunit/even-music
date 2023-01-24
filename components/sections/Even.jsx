a9c83a8ac39907e3ad3084d1a442ae34ed9fc1d6
izzett222 <iizzeddin62@gmail.com>
2023-01-24 10:04:22 +0200

    feat: remove arbitrary value and add them to tailwind config

--- components/sections/Even.jsx
+++ components/sections/Even.jsx
@@ -4,21 +4,21 @@ import { Heading, Paragraph, Span } from "../ui/Typography";
 
 export default function Even() {
   return (
-    <div className="mt-[70px] sm:mt-[178px] xl:mt-[258px]">
-      <div className="relative w-full aspect-[60/23]">
-        <Image src="/svg/even.svg" fill className="" />
+    <div className="mt-17.5 sm:mt-44.5 xl:mt-64.5">
+      <div className="relative w-full aspect-logo">
+        <Image src="/svg/even.svg" fill className="" alt="logo" />
       </div>
-      <div className="max-w-[350px] sm:max-w-[452.22px] mx-auto sm:mx-0 xl:max-w-[638px] mt-10 relative">
+      <div className="max-w-87.5 sm:max-w-113.05 mx-auto sm:mx-0 xl:max-w-159.5 mt-10 relative">
         <div className="font-medium text-lg sm:text-1xl xl:text-3.5xl -tracking-thinnest sm:-tracking-thinner xl:-tracking-thin leading-5.5 sm:leading-7 xl:leading-10 text-primary">
           A decentralised protocol for music artists, fans and{" "}
           <div className="text-accent underline  decoration-4 underline-offset-2 relative inline-block">
             collective forms
-            <div className="flex flex-col left-[0.8rem] sm:left-[1.4rem] xl:left-[4.3rem] items-center absolute translate-y-full bottom-0 xl:bottom-[3px]">
-              <div className="w-2.5 h-[60.9px] sm:h-[71.9px] relative xl:h-[83px]">
-                <Image src="/svg/line.svg" fill className="" />
+            <div className="flex flex-col left-3.2 sm:left-5.6 xl:left-18.4 items-center absolute translate-y-full bottom-0 xl:bottom-0.75">
+              <div className="w-2.5 h-15.22 sm:h-17.9 relative xl:h-20.75">
+                <Image src="/svg/line.svg" fill className="" alt="line" />
               </div>
               <Span>
-                <span className="text-accent leading-4.25 block  mx-auto min-w-[181px] sm:min-w-[213px] text-center">
+                <span className="text-accent leading-4.25 block  mx-auto min-w-45.25 sm:min-w-53.25 text-center">
                   The immaterial and collaborative nature of music productions
                   need flexible models
                 </span>
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,9 +16,21 @@ module.exports = {
         sans: ["var(--font-inter)", ...fontFamily.sans],
       },
       spacing: {
+        0.75: "0.1875rem", // 3px
+        3.2: "0.8rem", // 12.8px
+        5.6: "1.4rem", // 22.4px
+        15.22: "3.8062rem", // 60.9px
+        17.2: "4.3rem", // 68.8px
         17.5: '4.375rem',//70px,
+        17.9: "4.4938rem", // 71.9px
+        18.4: "4.6rem", // 73.6px
+        20.75: "5.1875rem", // 83px
         44.5: '11.125rem',// 178px
+        45.25: "11.3125rem", // 181px
+        53.25: "13.3125rem", // 213px
         64.5: '16.125rem',//258px
+
+
 
       },
       fontSize: {
@@ -39,7 +51,17 @@ module.exports = {
       },
       maxWidth: {
         "6xl": "70rem",
+        87.5: "21.875rem", // 350px
+        113.05: "28.2638rem", // 452.22px
+        159.5: "39.875rem", // 638px
       },
+      minWidth: {
+        45.25: "11.3125rem", // 181px
+        53.25: "13.3125rem", // 213px
+      },
+      aspectRatio: {
+        logo: "60/23"
+      }
     },
   },
   plugins: [],
