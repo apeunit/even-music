7a7de9afc4d4539d8a97dce9b943909f90978a19
Nkaka23dev <nkakaeric96@gmail.com>
2023-01-24 19:51:40 +0200

    feat: add title and favicon

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,13 +2,20 @@ import Wrapper from "@/components/wrapper/Wrapper";
 import Description from "@/components/sections/Description";
 import Header from "@/components/layout/Header";
 import Even from "@/components/sections/Even";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <Wrapper>
-      <Header />
-      <Even />
-      <Description />
-    </Wrapper>
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <title>Even</title>
+      </Head>
+      <Wrapper>
+        <Header />
+        <Even />
+        <Description />
+      </Wrapper>
+    </>
   );
 }
