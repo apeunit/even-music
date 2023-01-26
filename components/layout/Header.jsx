02e0ff386126ea982674c74b14d4e806419b2933
izzett222 <iizzeddin62@gmail.com>
2023-01-26 19:40:58 +0200

    fix: start mobile on sm breakpoint

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -3,10 +3,10 @@ import Anchor from "@/components/ui/Anchor";
 
 export default function Header() {
   return (
-    <div className="font-variation grid grid-cols-1 md:grid-cols-2 mb-13.25 md:mb-44 xl:mb-64.5">
-      <div className="md:col-start-2 flex flex-wrap md:flex-nowrap">
-        <div className="md:border-l-0.5 border-black pt-5 pr-0.5 md:pr-2.5 pb-4.25 md:pl-2.5 md:pt-10 md:pb-0.5 xl:pl-5 xl:pr-10 xl:pb-0">
-          <div className="max-w-48.25 md:max-w-54.75 pr-4.5 md:pr-3">
+    <div className="font-variation grid grid-cols-1 sm:grid-cols-2 mb-13.25 sm:mb-44 xl:mb-64.5">
+      <div className="sm:col-start-2 flex flex-wrap sm:flex-nowrap">
+        <div className="flex-1 sm:flex-initial sm:border-l-0.5 border-black pt-5 pr-0.5 sm:pr-2.5 pb-4.25 sm:pl-2.5 sm:pt-10 sm:pb-0.5 xl:pl-5 xl:pr-10 xl:pb-0">
+          <div className="max-w-48.25 sm:max-w-54.75 pr-4.5 sm:pr-3">
             <Span>
               Even wants to fundamentally change the way artists and their
               communities interact. Even aims to reshape the future of music
@@ -14,7 +14,7 @@ export default function Header() {
             </Span>
           </div>
         </div>
-        <div className="flex flex-col border-l-0.5 border-black pt-5 pl-5 gap-y-2.5 md:pl-2.5 md:pt-10 xl:pl-5 xl:gap-y-5">
+        <div className="flex-1 sm:flex-initial flex flex-col border-l-0.5 border-black pt-5 pl-5 gap-y-2.5 sm:pl-2.5 sm:pt-10 xl:pl-5 xl:gap-y-5">
           <Anchor url="https://evenmusic.co/">Litepaper</Anchor>
           <Anchor url="mailto:even@apeunit.com">Get In Touch</Anchor>
         </div>
