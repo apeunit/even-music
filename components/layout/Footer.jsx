b834964552910c24e5169520fd86eb2f8894721b
Odile078 <ouwimpuhwe620@daviscollege.com>
2023-01-26 20:37:52 +0200

    fix: modify footer padding on desktop

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -5,8 +5,8 @@ export default function Footer() {
   return (
     <div className="grid mt-20 font-variation sm:grid-cols-2 sm:mt-25">
       <div className="flex">
-        <div className="flex-1 xl:flex-initial pt-0.5 sm:pt-0.75 xl:pt-0 pl-0 xl:pl-10.5 xl:pr-16 pr-0 sm:pr-2">
-          <div className="max-w-none sm:max-w-48.5">
+        <div className="flex-1 xl:max-w-75 pt-0.5 sm:pt-0.75 xl:pt-0 pl-0 xl:pl-10.5 xl:pr-16 pr-0 sm:pr-2">
+          <div className="max-w-none sm:max-w-48.5 xl:w-full">
             <h5 className="font-variation-secondary block font-semibold sm:text-sm text-xs -tracking-tight leading-3.75 sm:leading-4.25 text-black">
               Even
             </h5>
@@ -14,7 +14,7 @@ export default function Footer() {
             <Span>10999 Berlin Germany</Span>
           </div>
         </div>
-        <div className="flex-1 sm:flex-initial border-l-0.5 border-black pt-1 pl-5 sm:pt-0.75 xl:pt-0 sm:pl-2.5 xl:pl-5 pr-0 sm:pr-2.5 xl:pr-0">
+        <div className="flex-1 sm:flex-initial xl:flex-1 border-l-0.5 border-black pt-1 pl-5 sm:pt-0.75 xl:pt-0 sm:pl-2.5 xl:pl-5 pr-0 sm:pr-2.5 xl:pr-0">
           <div className="max-w-33 sm:max-w-38.75">
             <h5 className="font-variation-secondary block font-semibold sm:text-sm text-xs -tracking-tight leading-3.75 sm:leading-4.25 text-black">
               Miguel Lizio
