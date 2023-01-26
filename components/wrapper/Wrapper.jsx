81f2fc43bddde02a94a7e6c165f6135d6a901c08
izzett222 <iizzeddin62@gmail.com>
2023-01-26 15:10:29 +0200

    fix: modify header description right padding

--- components/wrapper/Wrapper.jsx
+++ components/wrapper/Wrapper.jsx
@@ -1,6 +1,6 @@
 export default function Wrapper({ children }) {
   return (
-    <div className="px-5 sm:px-10 xl:px-20">
+    <div className="px-5 md:px-10 xl:px-20">
       <div className="max-w-6xl mx-auto">{children}</div>
     </div>
   );
