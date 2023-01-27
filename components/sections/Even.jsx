2e503a1959723b309e52964bd58b4dcabb1226b0
Nkaka23dev <nkakaeric96@gmail.com>
2023-01-27 15:32:47 +0200

    fix: fix even text padding right

--- components/sections/Even.jsx
+++ components/sections/Even.jsx
@@ -7,7 +7,7 @@ export default function Even() {
       <div className="relative w-full aspect-logo">
         <Image src="/svg/even.svg" fill alt="logo" priority />
       </div>
-      <div className="max-w-87.5 sm:max-w-113.05 mx-auto sm:mx-0 xl:max-w-159.5 mt-10 relative">
+      <div className="max-w-87.5 sm:max-w-113.05 mx-auto sm:mx-0 xl:max-w-159.5 mt-10 relative pr-[16px] sm:pr-0">
         <div className="font-medium text-lg sm:text-1xl xl:text-3.5xl -tracking-thinnest sm:-tracking-thinner xl:-tracking-thin leading-5.5 sm:leading-7 xl:leading-10 text-primary">
           A decentralised protocol for music artists, fans and&nbsp;
           <div className="text-secondary underline  relative inline-block">
@@ -18,7 +18,7 @@ export default function Even() {
               <div className="w-2.5 h-15.22 sm:h-17.9 relative xl:h-20.75">
                 <Image src="/svg/line.svg" fill alt="line" />
               </div>
-              <div className="mx-auto min-w-45.25 sm:min-w-53.25 text-center font-variation">
+              <div className="mx-auto min-w-45.25 sm:min-w-53.25 text-center font-variation  ">
                 <Span secondary>
                     The immaterial and collaborative nature of music productions
                     need flexible models
