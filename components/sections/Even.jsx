6128da8123b7b7627106894127b3c196ae05898a
Fidesnoella <fniragena@gmail.com>
2023-02-03 15:27:31 +0200

    feat:add max width on text in even-section on small screen

--- components/sections/Even.jsx
+++ components/sections/Even.jsx
@@ -7,7 +7,7 @@ export default function Even() {
       <div className="relative w-full aspect-logo">
         <Image src="/svg/even.svg" fill alt="logo" priority />
       </div>
-      <div className="max-w-87.5 sm:max-w-113.05 mx-auto sm:mx-0 xl:max-w-159.5 mt-10 relative pr-4 sm:pr-0">
+      <div className="max-w-62.5 xs:max-w-87.5 sm:max-w-113.05 mx-auto sm:mx-0 xl:max-w-159.5 mt-10 relative pr-4 sm:pr-0">
         <div className="font-normal text-lg sm:text-1xl xl:text-3.5xl -tracking-thinnest sm:-tracking-thinner xl:-tracking-thin leading-5.5 sm:leading-6.96 xl:leading-10 text-primary">
           A decentralised protocol for music artists, fans and&nbsp;
           <div className="text-secondary underline relative inline-block">
