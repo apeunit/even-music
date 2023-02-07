2af44ae0cc3372b9bdac951d074be96ed26c6435
izzett222 <iizzeddin62@gmail.com>
2023-02-07 15:11:06 +0200

    fix: remove base font size on the html element

--- components/ui/Anchor.jsx
+++ components/ui/Anchor.jsx
@@ -5,7 +5,7 @@ export default function Anchor({ children, url }) {
     <Link
       href={url}
       target="_blank"
-      className="inline-block font-normal sm:text-sm text-xs -tracking-tight sm:leading-4.25 leading-3.63 text-black underline underline-offset-3 hover:text-secondary"
+      className="inline-block font-normal sm:text-sm md:text-3.93 lg:text-4.37 xl:text-4.81 text-xs -tracking-0.1 md:-tracking-0.11 md:leading-4.76 lg:-tracking-0.125 lg:leading-5.29 xl:-tracking-0.13 xl:leading-5.82 sm:leading-4.25 leading-3.63 text-black underline underline-offset-3 hover:text-secondary"
     >
       {children}
     </Link>
