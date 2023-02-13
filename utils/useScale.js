afa896e80d2ff0e8ef6fe33379d126f9ea2c9018
izzett222 <iizzeddin62@gmail.com>
2023-02-13 15:10:08 +0200

    add font featfix: ure settings

--- utils/useScale.js
+++ utils/useScale.js
@@ -3,8 +3,8 @@ import { useEffect, useState } from "react";
 const useScale = () => {
   const [width, setWidth] = useState(0);
   let scale =  width / 1024;
-  if (scale > 1.40625) {
-    scale = 1.40625;
+  if (scale > 1.45625) {
+    scale = 1.45625;
   }
   useEffect(() => {
     setWidth(window.innerWidth);
