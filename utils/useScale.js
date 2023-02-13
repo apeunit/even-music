0cd6f5c87a6db10bac78a3ce8dc1092f70c6df43
izzett222 <iizzeddin62@gmail.com>
2023-02-13 14:21:06 +0200

    fix: set a max scale value to 140625

--- utils/useScale.js
+++ utils/useScale.js
@@ -2,7 +2,10 @@ import { useEffect, useState } from "react";
 
 const useWidth = () => {
   const [width, setWidth] = useState(0);
-  let scale = width / 1024;
+  let scale =  width / 1024;
+  if (scale > 1.40625) {
+    scale = 1.40625;
+  }
   useEffect(() => {
     setWidth(window.innerWidth);
     const handleWidth = () => {
@@ -13,7 +16,6 @@ const useWidth = () => {
       window.removeEventListener("resize", handleWidth);
     };
   }, []);
-  console.log(width, scale)
   return scale;
 }
 export default useWidth;
