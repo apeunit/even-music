2a056318501e80b45fce459386ac2e63d59386ed
Odile078 <ouwimpuhwe620@daviscollege.com>
2023-02-14 17:22:13 +0200

    refactor: remove unnecessary class,font-variation-secondary

--- components/sections/Even.jsx
+++ components/sections/Even.jsx
@@ -10,7 +10,7 @@ export default function Even() {
       <div className="max-w-62.5 xs:max-w-87.5 sm:max-w-113.05 md:max-w-127.18 lg:max-w-141.31 xl:max-w-219.31 mx-auto sm:mx-0 mt-10 relative pr-4 sm:pr-0">
         <div className="font-normal text-lg sm:text-5.75 md:text-6.46 lg:text-7.18 xl:text-11 -tracking-0.25 sm:-tracking-0.34 leading-5.5 sm:leading-6.96 md:-tracking-0.38 md:leading-7.83 lg:-tracking-0.42 lg:leading-8.7 xl:-tracking-0.61 xl:leading-13.75 text-primary">
           A decentralised protocol for music artists, fans and&nbsp;
-          <div className="text-secondary underline relative inline-block">
+          <div className="relative inline-block underline text-secondary">
             <span className="inline-block w-max underline decoration-0.475 sm:decoration-0.8 underline-offset-2 sm:underline-offset-1.25 xl:underline-offset-2">
               collective forms
             </span>
@@ -18,7 +18,7 @@ export default function Even() {
               <div className="w-2.5 h-15.22 sm:h-17.9 relative xl:h-20.75">
                 <Image src="/svg/line.svg" fill alt="line" />
               </div>
-              <div className="mx-auto min-w-45.25 sm:min-w-53.25 md:min-w-59.9 lg:min-w-66.56 xl:min-w-73.19 text-center font-variation">
+              <div className="mx-auto min-w-45.25 sm:min-w-53.25 md:min-w-59.9 lg:min-w-66.56 xl:min-w-73.19 text-center ">
                 <Span secondary>
                   The immaterial and collaborative nature of music productions
                   need flexible models
--- components/wrapper/Wrapper.jsx
+++ components/wrapper/Wrapper.jsx
@@ -1,13 +1,9 @@
-
 export default function Wrapper({ children }) {
-
   return (
     <div className="px-5 sm:px-10 xl:px-20">
-      <div className="max-w-7xl mx-auto">
+      <div className="mx-auto max-w-7xl font-variation">
         <div>
-          <div >
-            {children}
-          </div>
+          <div>{children}</div>
         </div>
       </div>
     </div>
