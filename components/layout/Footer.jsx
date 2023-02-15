4f61f49c6e7e61d0774e385e05e70c44d9bc466d
Nkaka23dev <nkakaeric96@gmail.com>
2023-02-15 11:20:28 +0200

    fix: adjust maximum width of the header and footer

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -8,7 +8,7 @@ export default function Footer() {
     <div className="grid mt-20 sm:grid-cols-2 sm:mt-25">
       <div className="grid grid-cols-2 sm:flex 1.5lg:grid xl:flex 1.5xl:grid">
         <div className="flex-1 xl:flex-initial pt-0.5 sm:pt-0.75 xl:pt-0 pl-0 pr-0 sm:pr-2">
-          <div className="max-w-none sm:max-w-48.5 md:max-w-54.56 lg:max-w-60.62 xl:max-w-66.68 xl:w-full">
+          <div className="max-w-none sm:max-w-48.5 md:max-w-54.56 lg:max-w-60.62 xl:max-w-[18rem] xl:w-full">
             <h5
               className="block font-semibold sm:text-sm text-xs -tracking-0.1 text-primary text-opacity-95 leading-0.30"
               style={
@@ -27,7 +27,7 @@ export default function Footer() {
           </div>
         </div>
         <div className="flex-1 sm:flex-initial xl:flex-1 border-l-0.5 border-black pt-1 pl-5 sm:pt-0.75 xl:pt-0 sm:pl-2.5 xl:pl-5 pr-0 sm:pr-2.5 xl:pr-0">
-          <div className="max-w-33 sm:max-w-38.75 md:max-w-43.59 lg:max-w-48.43 xl:max-w-53.28">
+          <div className="max-w-33 sm:max-w-38.75 md:max-w-43.59 lg:max-w-48.43 xl:max-w-[15rem]">
             <h5
               className="block font-semibold sm:text-sm text-xs -tracking-0.1 text-primary text-opacity-95 leading-0.30"
               style={
@@ -47,7 +47,7 @@ export default function Footer() {
       </div>
       <div className="grid grid-cols-2 sm:flex 1.5lg:grid xl:flex">
         <div className="flex-1 lg:flex-initial xl:pr-10 sm:border-l-0.5 border-black pt-5 pb-5 sm:pb-10 sm:pt-0.75 xl:pt-0 pr-1 xs:pr-0 sm:pr-2.5 pl-0 sm:pl-2.5 xl:pl-5">
-          <div className="max-w-39.5 sm:max-w-54.75 md:max-w-61.59 lg:max-w-68.34 lg:pr-1.5 xl:max-w-75.28 xl:w-full">
+          <div className="max-w-39.5 sm:max-w-54.75 md:max-w-61.59 lg:max-w-68.34 lg:pr-1.5 xl:max-w-[22rem] xl:w-full">
             <Span>
               We are developers, designers, researchers and music enthusiast
               interested in building decentralised systems, fostering open
@@ -55,7 +55,7 @@ export default function Footer() {
             </Span>
           </div>
         </div>
-        <div className="flex-1 sm:flex-initial flex flex-col justify-between border-l-0.5 border-black pl-5 pb-5 pt-0 sm:pt-0.5 xl:pt-0 sm:pl-2.5 xl:pl-5 sm:pb-10.75 xl:pb-10 pr-0 sm:pr-9.75 xl:pr-0">
+        <div className="flex-1 sm:flex-initial flex flex-col justify-between border-l-0.5 border-black pl-5 pb-5 pt-0 sm:pt-0.5 xl:pt-0 sm:pl-2.5 xl:pl-5 sm:pb-10.75 xl:pb-10 pr-0 sm:pr-9.75 xl:pr-0 ">
           <Anchor url="mailto:even@apeunit.com">Get In Touch</Anchor>
           <div>
             <span
