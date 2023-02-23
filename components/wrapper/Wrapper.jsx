c35889a59533f49dc6d48c71a87c85cf69ecddd6
izzett222 <iizzeddin62@gmail.com>
2023-02-23 12:27:44 +0200

    fix: remove more unnecessary div

--- components/wrapper/Wrapper.jsx
+++ components/wrapper/Wrapper.jsx
@@ -1,9 +1,7 @@
 export default function Wrapper({ children }) {
   return (
     <div className="px-5 sm:px-10 xl:px-20 selection:bg-lightGrey">
-      <div className="mx-auto max-w-7xl font-variation">
-          <div>{children}</div>
-      </div>
+      <div className="mx-auto max-w-7xl font-variation">{children}</div>
     </div>
   );
 }
